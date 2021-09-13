f293b3fd74bd77d81dc15aff892e54a420e1477e
ponnhide <morityun@sfc.keio.ac.jp>
2021-09-13 19:21:03 +0900

    Update .gitignore

--- QUEEN/qint.py
+++
@@ -1,7 +0,0 @@
-class Qint(int):
-    def __init__(self, num):
-        self.qkey        = None
-        self.parent      = None
-        self.parental_id = None
-        self.name        = None
-        self._qint       = True

461690c56de1a77cf5248600781670ef2e582457
Hideto Mori <hideto@Hidetos-Mac-Studio.local>
2025-05-14 13:03:46 +0900

    bug fix in handling a feature with CompoundLocation

--- QUEEN/qfunction.py
+++ QUEEN/qfunction.py
@@ -936,7 +936,7 @@ def cutdna(dna, *cutsites, crop=False, supfeature=False, product=None, process_n
                         else:
                             locations[0][0] = ExactPosition(s - start)
                         
-                        if e > end-start and eflag == 1:
+                        if e-start > end-start and eflag == 1:
                             locations[-1][1] = ExactPosition(end-start)
                             if "broken_feature" not in feat.qualifiers:
                                 label = feat._id 
